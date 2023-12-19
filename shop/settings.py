5dad60a5dd7b94338e77787c12bb1e5b868aecaa
kevinlin <a0932687402@gmail.com>
2023-12-19 12:39:43 +0800

    12/19 寫入log

--- shop/settings.py
+++ shop/settings.py
@@ -74,6 +74,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'shop.wsgi.application'
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django.log',  # Specify the log file path
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
